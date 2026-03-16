2c63f4b1918b0012ade2c9484cc188fbd8e451a2
Gulshan Kumar <gulshankumar3142@gmail.com>
2026-03-16 21:44:29 +0530

    Add number to compare two integers equal are not

---
+++ compare_number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+int main(){
+   int a,b,temp;
+   printf("Enter value of a & b : ");
+   scanf("%d %d",&a,&b);
+
+  if( a==b){
+    printf("a and b are equal");
+  }
+  else{
+     printf("a and b are not equal");
+  }
+    
+   return 0;
+}
